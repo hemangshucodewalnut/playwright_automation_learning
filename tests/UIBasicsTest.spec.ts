4a904184dccdd515408cdcdec49b1a28998c7612
hemangshucodewalnut <hemangshu@codewalnut.com>
2026-02-26 14:00:06 +0530

    feat: typing into elements

--- tests/UIBasicsTest.spec.ts
+++ tests/UIBasicsTest.spec.ts
@@ -4,6 +4,9 @@ test("Browser Context Playwright test", async ({ browser }) => {
 	const context = await browser.newContext();
 	const page = await context.newPage();
 	await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+	await page.locator("#username").fill("rahulshettyacademy");
+	await page.locator('[type="password"]').fill("Learning@830$3mK2");
+	await page.locator("#signInBtn").click();
 });
 
 test("Page Fixture Test", async ({ page }) => {
