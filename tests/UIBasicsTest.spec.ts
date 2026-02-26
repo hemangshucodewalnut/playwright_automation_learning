b0b6a89f83325de363b41001d3bae87cb073aefa
hemangshucodewalnut <hemangshu@codewalnut.com>
2026-02-26 14:43:41 +0530

    feat: work with locators which extract multiple webelements in page

--- tests/UIBasicsTest.spec.ts
+++ tests/UIBasicsTest.spec.ts
@@ -1,12 +1,39 @@
 import { expect, test } from "@playwright/test";
 
-test("Browser Context Playwright test", async ({ browser }) => {
+test("should show error message for invalid login", async ({ browser }) => {
 	const context = await browser.newContext();
 	const page = await context.newPage();
+
+	const userName = page.locator("#username");
+	const password = page.locator('[type="password"]');
+	const signin = page.locator("#signInBtn");
+
 	await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-	await page.locator("#username").fill("abcd");
-	await page.locator('[type="password"]').fill("Learning");
-	await page.locator("#signInBtn").click();
-	console.log(await page.locator("[style*=block]").textContent());
+
+	await userName.fill("abcd");
+	await password.fill("Learning@830$3mK2");
+	await signin.click();
+
 	await expect(page.locator("[style*=block]")).toContainText("Incorrect");
+});
+
+test("should grab text content of first card after valid login", async ({
+	browser,
+}) => {
+	const context = await browser.newContext();
+	const page = await context.newPage();
+
+	const userName = page.locator("#username");
+	const password = page.locator('[type="password"]');
+	const signin = page.locator("#signInBtn");
+	const cardTitles = page.locator(".card-body a");
+
+	await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+	await userName.fill("rahulshettyacademy");
+	await password.fill("Learning@830$3mK2");
+	await signin.click();
+
+	const firstCardText = await cardTitles.nth(0).textContent();
+	console.log(firstCardText);
 });
