3fa7277e00916194b2cd0c15e4798dc9e551c1cc
hemangshucodewalnut <hemangshu@codewalnut.com>
2026-02-27 13:34:36 +0530

    test(e2e): add flow to find specific product dynamically from products list and add it to cart

--- tests/PlaceOrder.spec.ts
+++ tests/PlaceOrder.spec.ts
@@ -1,8 +1,19 @@
 import { test, expect } from "@playwright/test";
 
 test("should place an order", async ({ page }) => {
+	const productName = "ZARA COAT 3";
+	const products = page.locator(".card-body");
 	await page.goto("https://rahulshettyacademy.com/client/");
 	await page.locator("#userEmail").fill("hemangshu@yahoo.com");
 	await page.locator("#userPassword").fill("1234@Asdf");
 	await page.locator('[value="Login"]').click();
+	await page.waitForLoadState("networkidle");
+	await page.locator(".card-body b").first().waitFor();
+	const productCount = await products.count();
+	for (let i = 0; i < productCount; ++i) {
+		if ((await products.nth(i).locator("b").textContent()) === productName) {
+			await products.nth(i).locator("text= Add To Cart").click();
+			break;
+		}
+	}
 });
