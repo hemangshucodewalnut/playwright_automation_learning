c538c53e4e4c15b78709e027816354d9cb71e407
hemangshucodewalnut <hemangshu@codewalnut.com>
2026-02-27 14:32:27 +0530

    test(e2e): add flow to place the order

--- tests/PlaceOrder.spec.ts
+++ tests/PlaceOrder.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from "@playwright/test";
 
 test("should place an order", async ({ page }) => {
+	const email = "hemangshu@yahoo.com";
 	const productName = "ZARA COAT 3";
 	const products = page.locator(".card-body");
 	await page.goto("https://rahulshettyacademy.com/client/");
-	await page.locator("#userEmail").fill("hemangshu@yahoo.com");
+	await page.locator("#userEmail").fill(email);
 	await page.locator("#userPassword").fill("1234@Asdf");
 	await page.locator('[value="Login"]').click();
 	await page.waitForLoadState("networkidle");
@@ -39,5 +40,12 @@ test("should place an order", async ({ page }) => {
 			break;
 		}
 	}
-	await page.pause();
+	expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+	await page.locator("text=Place Order").click();
+	await expect(page.locator(".hero-primary")).toHaveText(
+		" Thankyou for the order. ",
+	);
+	const orderId = await page
+		.locator(".em-spacer-1 .ng-star-inserted")
+		.textContent();
 });
