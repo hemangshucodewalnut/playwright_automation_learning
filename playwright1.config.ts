7628e443b052d858bcb5d91fc72d988e330d8aeb
hemangshucodewalnut <hemangshu@codewalnut.com>
2026-03-10 11:05:41 +0530

    config: different projects configurations

---
+++ playwright1.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+	testDir: "./tests",
+	timeout: 40 * 1000,
+	expect: {
+		timeout: 5 * 1000,
+	},
+	reporter: "html",
+
+	projects: [
+		{
+			name: "chromium",
+			use: {
+				browserName: "chromium",
+				headless: true,
+				// viewport: { width: 1280, height: 720 },
+			},
+		},
+		{
+			name: "safari",
+			use: {
+				browserName: "webkit",
+				headless: true,
+				// ...devices["iPhone 12"],
+				// ignoreHTTPSErrors: true,
+				// permissions: ["geolocation"],
+			},
+		},
+	],
+});
--- tests/PlaceOrderPO.spec.ts
+++ tests/PlaceOrderPO.spec.ts
@@ -63,62 +63,68 @@ test("should place an order", async ({ page }) => {
 	expect(orderId.includes(orderIdDetails)).toBeTruthy();
 });
 
-customTest.only("should place an order", async ({ page, testDataProduct }) => {
-	const poManager = new POManager(page);
-	const loginPage = poManager.getLoginPage();
-	const dashboardPage = poManager.getDashboardPage();
-	const email = testDataProduct.email;
-	const password = testDataProduct.password;
-	const productName = testDataProduct.productName;
+customTest(
+	"should place an order with custom test data",
+	async ({ page, testDataProduct }) => {
+		const poManager = new POManager(page);
+		const loginPage = poManager.getLoginPage();
+		const dashboardPage = poManager.getDashboardPage();
+		const email = testDataProduct.email;
+		const password = testDataProduct.password;
+		const productName = testDataProduct.productName;
 
-	await loginPage.goTo();
-	await loginPage.validLogin(email, password);
+		await loginPage.goTo();
+		await loginPage.validLogin(email, password);
 
-	dashboardPage.searchProduct(productName);
-	await dashboardPage.goToCart();
+		dashboardPage.searchProduct(productName);
+		await dashboardPage.goToCart();
 
-	await page.locator("div li").first().waitFor();
-	const isProductVisibleInCart = await page
-		.locator(`h3:has-text("${productName}")`)
-		.isVisible();
-	expect(isProductVisibleInCart).toBeTruthy();
+		await page.locator("div li").first().waitFor();
+		const isProductVisibleInCart = await page
+			.locator(`h3:has-text("${productName}")`)
+			.isVisible();
+		expect(isProductVisibleInCart).toBeTruthy();
 
-	await page.locator("text=Checkout").click();
-	await page
-		.locator("[placeholder*='Country']")
-		.pressSequentially("ind", { delay: 150 });
-	const dropdownOptions = page.locator(".ta-results");
-	await dropdownOptions.waitFor();
-	const dropdownOptionsCount = await dropdownOptions.locator("button").count();
+		await page.locator("text=Checkout").click();
+		await page
+			.locator("[placeholder*='Country']")
+			.pressSequentially("ind", { delay: 150 });
+		const dropdownOptions = page.locator(".ta-results");
+		await dropdownOptions.waitFor();
+		const dropdownOptionsCount = await dropdownOptions
+			.locator("button")
+			.count();
 
-	for (let i = 0; i < dropdownOptionsCount; ++i) {
-		const text = await dropdownOptions.locator("button").nth(i).textContent();
-		if (text === " India") {
-			await dropdownOptions.locator("button").nth(i).click();
-			break;
+		for (let i = 0; i < dropdownOptionsCount; ++i) {
+			const text = await dropdownOptions.locator("button").nth(i).textContent();
+			if (text === " India") {
+				await dropdownOptions.locator("button").nth(i).click();
+				break;
+			}
 		}
-	}
-	expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
-	await page.locator("text=Place Order").click();
-	await expect(page.locator(".hero-primary")).toHaveText(
-		" Thankyou for the order. ",
-	);
-	const orderId: string =
-		(await page.locator(".em-spacer-1 .ng-star-inserted").textContent()) || "";
+		expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+		await page.locator("text=Place Order").click();
+		await expect(page.locator(".hero-primary")).toHaveText(
+			" Thankyou for the order. ",
+		);
+		const orderId: string =
+			(await page.locator(".em-spacer-1 .ng-star-inserted").textContent()) ||
+			"";
 
-	await page.locator("button[routerlink*='myorders']").click();
-	await page.locator("tbody").waitFor();
-	const orderDetailsRows = page.locator("tbody tr");
+		await page.locator("button[routerlink*='myorders']").click();
+		await page.locator("tbody").waitFor();
+		const orderDetailsRows = page.locator("tbody tr");
 
-	for (let i = 0; i < (await orderDetailsRows.count()); ++i) {
-		const productIdsInOrderDetailsTable: string =
-			(await orderDetailsRows.nth(i).locator("th").textContent()) || "";
-		if (orderId.includes(productIdsInOrderDetailsTable)) {
-			await orderDetailsRows.nth(i).locator("button").first().click();
-			break;
+		for (let i = 0; i < (await orderDetailsRows.count()); ++i) {
+			const productIdsInOrderDetailsTable: string =
+				(await orderDetailsRows.nth(i).locator("th").textContent()) || "";
+			if (orderId.includes(productIdsInOrderDetailsTable)) {
+				await orderDetailsRows.nth(i).locator("button").first().click();
+				break;
+			}
 		}
-	}
-	const orderIdDetails: string =
-		(await page.locator(".col-text").textContent()) || "";
-	expect(orderId.includes(orderIdDetails)).toBeTruthy();
-});
+		const orderIdDetails: string =
+			(await page.locator(".col-text").textContent()) || "";
+		expect(orderId.includes(orderIdDetails)).toBeTruthy();
+	},
+);
